451691d835ff44431405afb6cf545f815f9034d9
Mario <g.v.mario96@gmail.com>
2024-10-21 17:08:20 +0200

    Subida del proyecto a github
    Primera subida con el proyecto creado con Spring Initializr

---
+++ calendario_v2/src/main/java/es/plexus/calendario_v2/CalendarioV2Application.java
@@ -0,0 +1,13 @@
+package es.plexus.calendario_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarioV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarioV2Application.class, args);
+	}
+
+}
